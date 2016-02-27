46901bdf32c84ca625aa6f883f4cfa4adec4ad25
mstrama <marta_strama@wp.pl>
2016-02-27 17:16:43 +0100

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,55 @@
+<html>
+<meta charset="windows-1250"/>
+<meta charset="ISO-8859-2"/>
+<meta charset="UTF-8"/>
+<head>
+<title>
+ Speed &#9762
+</title>
+
+<style>
+h1 {font-size: 55px; display: inline; text-indent: 600; word-spacing: 20;margin-left: 150}
+h2 {text-align: center;}
+p {font-size: 15px; font-style: oblique; font-weight: italic; font-weight: bold;}
+</style>
+<link rel="Stylesheet" type="text/css" href="https://v-ie.uek.krakow.pl/~s176968/style2.css" />
+</head>
+<body>
+
+ <div id="container1">
+ <div id="title">
+ <h1><font face="fantasy" color="#8C1717" size="10"> Wypożyczalnia samochodów</font></h1>
+ </div>
+ <div id="login">
+		<h2><u>Logowanie użytkownika</u></br></h2>
+		<p><form method="POST" action="log.php"></p>
+		<p>Login:<input type="text" name="login"
+		style="background-color: #BDB76B"/></p>
+		<p>Hasło:<input type="password" name="haslo" style="background-color: #BDB76B"/>
+		<input type="image" width="40" height="40" align="right" hspace="20" src="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/simple-red-square-icons-business/128632-simple-red-square-icon-business-key11-sc48.png"/>
+		</p></form>
+		
+ </div>
+</div>
+
+<div id="container2">
+<div id="main">
+ <ul id="menu">
+ <li><a href="https://v-ie.uek.krakow.pl/~s176968/main2.php">Strona główna</a></li>
+ <li><a href="https://v-ie.uek.krakow.pl/~s176968/sklep2.php">Sklep</a></li>
+ <li><a href="https://v-ie.uek.krakow.pl/~s176968/koszyk2.php">Koszyk</a></li>
+ <li><a href="https://v-ie.uek.krakow.pl/~s176968/kontakt2.php">Kontakt</a></li>
+</ul></br>
+<div id="two">dalsza cześć menu
+</div>
+</div>
+<div id="reklama">
+<a href="https://v-ie.uek.krakow.pl/~s176968/rejestracja.php"><b>Rejestracja użytkownika</b></a>
+</div>
+</div>
+
+<div id="stopka">
+<h2>Speed <sup>&reg</sup></h2>
+</div>
+</body>
+</html>
